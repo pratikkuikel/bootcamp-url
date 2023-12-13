4fd61c9cdf7f5c52ab158c6279cc2ecd0e5ce172
pratik kuikel <35728183+pratikkuikel@users.noreply.github.com>
2023-12-13 18:39:05 +0545

    cache

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -13,20 +13,45 @@ use App\Events\UrlCreation;
 use App\Mail\UrlCreatedMail;
 use App\Mail\UrlCreatedMarkdownMail;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Cache;
 
 class UrlController extends Controller
 {
     public function index()
     {
+        // $value = Cache::set('key','hello');
+        // dd(Cache::get('key'));
         // abort(404);
         // $urls = Url::all();
         // get urls created by currently authenticated user
         // $urls = auth()->user()->urls;
         // $userId = auth()->user()->id;
         // $urls = Url::where('user_id', $userId)->get();
-        $user_id = auth()->id();
         // Log::error($user);
-        $urls = Url::where('user_id', $user_id)->paginate(50);
+
+
+        // if there is no urls key in cache fetch urls from the database
+        // fetch urls from database and set the urls key
+        // else fetch the urls from cache and return it
+
+
+        // if (Cache::has('urls')) {
+        //     Log::info('urls present');
+        //     $urls =  Cache::get('urls');
+        //     Cache::forget('urls');
+        //     return $urls;
+        // } else {
+        //     Log::info('urls not present in the cache');
+        //     $urls = Url::where('user_id', $user_id)->get();
+        //     Cache::set('urls', $urls);
+        //     return Cache::get('urls');
+        // }
+        $user_id = auth()->id();
+
+        $urls = Cache::remember('urls', 600, function () use ($user_id) {
+            return Url::where('user_id', $user_id)->paginate(50);
+        });
+
         $count = Url::where('user_id', $user_id)->count();
         return view('urls.index', compact('urls', 'count'));
     }
@@ -61,8 +86,8 @@ class UrlController extends Controller
         ]);
         $user = auth()->user();
         // Mail::to($user)->send(new UrlCreatedMail($url));
-        Mail::to($user)->send(new UrlCreatedMarkdownMail($url));
-        UrlCreation::dispatch($url);
+        // Mail::to($user)->send(new UrlCreatedMarkdownMail($url));
+        // UrlCreation::dispatch($url);
         return redirect()->action([UrlController::class, 'index']);
     }
 
@@ -90,6 +115,8 @@ class UrlController extends Controller
         $url->original_url = $request->url;
         $url->save();
 
+        Cache::forget('urls');
+
         $request->session()->flash('success', 'Url was updated successfully!');
 
         return redirect()->action([UrlController::class, 'index']);
@@ -102,6 +129,7 @@ class UrlController extends Controller
             abort(401);
         }
         $url->delete();
+        Cache::forget('urls');
         $request->session()->flash('success', 'Url was deleted successfully!');
         return redirect()->action([UrlController::class, 'index']);
     }
